f2a8548d5be69218f43705bc3f0381a558a6180d
Kinza Sheikh <75093626+KinzaaSheikh@users.noreply.github.com>
2024-07-01 00:22:51 +0500

    Typo fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import SearchTodos from "./components/TodoList"
 import SearchUsers from "./components/UsersList"
 
-
 function App() {
   return (
     <div className="App">
       <h2>Higher Order Component</h2>
-      <div className="sectiom">
+      <div className="section">
         <div>
           <SearchUsers />
         </div>
